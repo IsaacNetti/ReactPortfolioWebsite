b679717aadbb29b967ece40b013eab3a2c403b97
Isaac Netti <isaacnetti@gmail.com>
2024-08-15 14:58:37 -0400

    adding framer and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-
+import{motion} from "framer-motion";
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <motion.h1 className="text-3xl font-bold underline animate-pulse" >
+      Hello world!
+    </motion.h1>
     </>
   )
 }
