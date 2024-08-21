7b1fd7db1e38286f0b2258e4240b2882ef50aecf
IsaacNetti <isaacnetti@gmail.com>
2024-08-21 17:22:55 -0400

    Fixing smaller screen formatting

--- src/Components/AboutMe.tsx
+++ src/Components/AboutMe.tsx
@@ -28,8 +28,8 @@ export default function AboutMe() {
     }
     return (
         <>
-        <div className='block lg:flex flex-row-reverse relative border-black justify-around bg-rippedPaper bg-cover bg-no-repeat lg:w-screen h-screen '>
-            <div className='flex flex-col justify-center h-3/5 max-w-fit mx-auto sm:h-auto sm:mx-0 sm:mr-24'>
+        <div className='block md:flex flex-row-reverse relative border-black justify-around bg-rippedPaper bg-cover bg-no-repeat md:w-screen h-screen '>
+            <div className='flex flex-col justify-center h-3/5 max-w-fit mx-auto md:h-auto md:mx-0 md:mr-24'>
                 <motion.div animate="pulse" variants={variants} className='px-32 py-28 bg-mePaper bg-cover bg-no-repeat'></motion.div>
                 <div className='flex'>
                     <a href='https://github.com/IsaacNetti'><motion.div animate="hover" variants={variants} className='bg-githubPaper bg-cover bg-no-repeat px-16 py-14'></motion.div></a>
@@ -37,10 +37,10 @@ export default function AboutMe() {
                 </div>
             </div>
 
-            <div className='flex flex-col justify-center border-black sm:ml-16'>
-                <h1 className=' font-display self-center text-4xl sm:self-start sm:text-9xl '>Hi, my</h1>
-                <h1 className=' font-display self-center text-4xl sm:self-start sm:text-9xl' >name is <b>Isaac</b></h1>
-                <h2 className=' font-display self-center text-2xl sm:text-4xl'>I'm a React web-developer</h2>
+            <div className='flex flex-col justify-center border-black md:ml-16'>
+                <h1 className=' font-display self-center text-6xl md:self-start md:text-9xl '>Hi, my</h1>
+                <h1 className=' font-display self-center text-6xl md:self-start md:text-9xl' >name is <b>Isaac</b></h1>
+                <h2 className=' font-display self-center text-4xl'>I'm a React web-developer</h2>
             </div>
         </div>
         </>
